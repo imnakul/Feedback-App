d1224b285aabdef16f71e20f80fc643c198956b1
Nakul Srivastava <imnakul44@gmail.com>
2024-08-16 09:25:42 +0530

    feedbackdata+addedappState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackItem from "./components/FeedbackItem";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
+   const [feedback, setFeedback] = useState(FeedbackData);
    return (
       <>
          <Header />
