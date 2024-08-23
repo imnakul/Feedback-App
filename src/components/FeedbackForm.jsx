972cfed45c2b965f3ca65d286dc944352ae12035
Nakul Srivastava <imnakul44@gmail.com>
2024-08-23 21:57:05 +0530

    Rating Component - to select rating and SETTIng that state in feedback form

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,13 @@
 import Card from "./shared/Card";
 import { useState } from "react";
 import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
 function FeedbackForm() {
    const [text, setText] = useState("");
    const [btnDisabled, setBtnDisabled] = useState(true);
    const [message, setMessage] = useState("");
+   const [rating, setRating] = useState(10);
 
    const handleTextChange = (e) => {
       if (text === "") {
@@ -26,7 +28,7 @@ function FeedbackForm() {
       <Card>
          <form>
             <h2> How would you rate your service with us?</h2>
-            {/* @todo - rating select component */}
+            <RatingSelect select={(rating) => setRating(rating)} />
             <div className='input-group'>
                <input
                   onChange={handleTextChange}
