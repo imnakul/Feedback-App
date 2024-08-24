cb89dd5b24d456b1e08b048c1bbbd66a7d26044c
Nakul Srivastava <imnakul44@gmail.com>
2024-08-24 07:49:55 +0530

    Added Feedback to feedback list

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
    const [feedback, setFeedback] = useState(FeedbackData);
+
+   const addFeedback = (newFeedback) => {
+      newFeedback.id = uuidv4(); //to assign a unique id using uuid package
+      //console.log(newFeedback);
+      setFeedback([newFeedback, ...feedback]);
+   };
 
    const deleteFeedback = (id) => {
       if (window.confirm("Are you sure you want to delete?")) {
@@ -18,7 +25,7 @@ function App() {
       <>
          <Header />
          <div className='container'>
-            <FeedbackForm />
+            <FeedbackForm handleAdd={addFeedback} />
             <FeedbackStats feedback={feedback} />
             <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
          </div>
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Button from "./shared/Button";
 import RatingSelect from "./RatingSelect";
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
    const [text, setText] = useState("");
    const [btnDisabled, setBtnDisabled] = useState(true);
    const [message, setMessage] = useState("");
@@ -24,9 +24,21 @@ function FeedbackForm() {
       setText(e.target.value);
    };
 
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      if (text.trim().length > 10) {
+         const newFeedback = {
+            text,
+            rating,
+         };
+         handleAdd(newFeedback);
+         setText("");
+      }
+   };
+
    return (
       <Card>
-         <form>
+         <form onSubmit={handleSubmit}>
             <h2> How would you rate your service with us?</h2>
             <RatingSelect select={(rating) => setRating(rating)} />
             <div className='input-group'>
